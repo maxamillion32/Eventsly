05149df7b89a8f497beecbd7737bbe629315ba7b
William <wt00863@georgiasouthern.edu>
2015-04-15 02:14:25 -0400

    new changes

---
+++ app/src/main/java/com/example/william/eventsly/AttendeeEvents.java
@@ -0,0 +1,15 @@
+package com.example.william.eventsly;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class AttendeeEvents extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendee_events);
+    }
+}
