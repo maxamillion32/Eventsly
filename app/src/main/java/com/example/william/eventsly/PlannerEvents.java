3c49cc434e5843dff1745ef0df0f71692fd88d9b
William <wt00863@georgiasouthern.edu>
2015-04-15 02:14:31 -0400

    new changes

---
+++ app/src/main/java/com/example/william/eventsly/PlannerEvents.java
@@ -0,0 +1,15 @@
+package com.example.william.eventsly;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class PlannerEvents extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner_events);
+    }
+}
