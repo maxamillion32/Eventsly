fb808d82dd111309d5259a17b03986581ddbb69a
William <wt00863@georgiasouthern.edu>
2015-04-15 01:24:00 -0400

    new changes

---
+++ app/src/main/java/com/example/william/eventsly/Role.java
@@ -0,0 +1,26 @@
+package com.example.william.eventsly;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Role extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role);
+
+    }
+
+    public void onPlannerClick(View view)
+    {
+
+    }
+
+    public void onAttendeeClick(View view)
+    {
+
+    }
+}
